51b29a49705df09246af69717898f5e40ba7034d
asciiboi <56585404+asciiboi@users.noreply.github.com>
2023-05-18 14:27:43 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+
+function getLocation(callback) {
+    if (navigator.geolocation) {
+        return navigator.geolocation.getCurrentPosition(callback)
+    } else {
+        throw "Gelocation error"
+    }
+}
+
+function getTempAt(lat, lon) {
+    const Http = new XMLHttpRequest();
+    const url=`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`;
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        resp = JSON.parse(Http.responseText)
+        document.querySelector("#temp").innerText = resp.current_weather.temperature
+    }
+}
+
+getLocation((resp) => {
+    console.log(resp)
+    getTempAt(resp.coords.latitude, resp.coords.longitude)
+})
+
